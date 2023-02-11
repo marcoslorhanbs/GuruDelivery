94bcfc226609c75f6c248a0008c7787109498e67
Marcos Lorhan <marcoslorhanbs@gmail.com>
2023-02-11 08:56:44 -0300

    Add filters for AdminSys

--- GuruApp/admin.py
+++ GuruApp/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 from GuruApp.models import user, Hamburguer
 
 admin.site.register(user)
-admin.site.register(Hamburguer)
+#admin.site.register(Hamburguer)
+
+@admin.register(Hamburguer)
+class HamburguerAdmin(admin.ModelAdmin):
+    list_display = ("productName", "productLine")
+    list_filter = ("productPrice", )
+    search_fields = ("productName__startswith", )
--- GuruApp/models.py
+++ GuruApp/models.py
@@ -5,6 +5,9 @@ class user(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=250)
     name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Hamburguer(models.Model):
@@ -12,5 +15,6 @@ class Hamburguer(models.Model):
     productLine = models.CharField(max_length=250)
     productDescription = models.TextField(max_length=500)
     productPrice = models.FloatField()
-    
+    def __str__(self):
+        return f"{self.productName}"
     
